22a90f1dbd324eece5d0395ed61c5e5ba902939f
Adam Jurík <ja004@seznam.cz>
2024-07-12 11:59:30 +0200

    (00:47:48) Create a Confusion Matrix for Neural Network Predictions

--- pt1.py
+++ pt1.py
@@ -145,4 +145,47 @@ for i in rounded_predictions:
 for i in range(np.size(rounded_predictions)):
     print(str(test_samples[i]) + ": " + str(rounded_predictions[i]))
     
+# (00:47:48) Create a Confusion Matrix for Neural Network Predictions
+
+#%matplotlib inline
+from sklearn.metrics import confusion_matrix
+import itertools
+import matplotlib.pyplot as plt
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title= 'Confusion matrix' ,
+                          cmap=plt.cm.Blues) :
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting=True
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))    
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
     
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=l)[:, np.newaxis]
+        print("Norma1ized confusion matrix")
+    else: 
+        print('Confusion matrix, without normalization')
+        
+    print(cm)
+    
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+        
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+cm_plot_labels = ['no_side_effects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
