89e4b0e3733a26a03cc06e2150e6bcfbb085d663
Adam Jurík <ja004@seznam.cz>
2024-07-12 14:50:17 +0200

    (01:01:25) Image Preparation for CNNs with TensorFlow's Keras API
    https://www.kaggle.com/datasets/shaunthesheep/microsoft-catsvsdogs-dataset

---
+++ pt1_CatsVsDogs.py
@@ -0,0 +1,87 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.metrics import confusion_matrix
+import itertools
+import os
+import shutil
+import random
+import glob
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+#%matplotlib inline
+import matplotlib.pyplot as plt
+
+# fix from: https://stackoverflow.com/questions/48225729/importerrorcould-not-import-pil-image-working-with-keras-ternsorflow/52230898#52230898
+from sklearn.preprocessing import LabelEncoder
+# from tensorflow.keras.preprocessing.image import load_img
+
+# https://www.kaggle.com/datasets/shaunthesheep/microsoft-catsvsdogs-dataset
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(physical_devices))
+# too bad...I dont hgave Nvidia
+# tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+# Organize data into train, valid, test dirs
+os.chdir('data/dogs-vs-cats')
+if os.path.isdir('train/dog') is False:
+    os.makedirs('train/dog')
+    os.makedirs('train/cat')
+    os.makedirs('valid/dog')
+    os.makedirs('valid/cat')
+    os.makedirs('test/dog')
+    os.makedirs('test/cat')
+    
+    for c in random.sample(glob.glob('cat*'), 500):
+        shutil.move(c,'train/cat')
+    for c in random.sample(glob.glob('dog*'), 500):
+        shutil.move(c,'train/dog')
+    for c in random.sample(glob.glob('cat*'), 100):
+        shutil.move(c,'valid/cat')
+    for c in random.sample(glob.glob('dog*'), 100):
+        shutil.move(c,'valid/dog')
+    for c in random.sample(glob.glob('cat*'), 50):
+        shutil.move(c,'test/cat')
+    for c in random.sample(glob.glob('dog*'), 50):
+        shutil.move(c,'test/dog')
+    
+os.chdir('../../')
+
+train_path = 'data/dogs-vs-cats/train'
+valid_path = 'data/dogs-vs-cats/valid'
+test_path = 'data/dogs-vs-cats/test'
+
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=train_path, target_size=(224,224), classes=['cat', 'dog'], batch_size=10)
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=valid_path, target_size=(224,224), classes=['cat', 'dog'], batch_size=10)
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input) \
+    .flow_from_directory(directory=test_path, target_size=(224,224), classes=['cat', 'dog'], batch_size=10, shuffle=False)
+
+assert train_batches.n == 1000
+assert valid_batches.n == 1000
+assert test_batches.n == 1000
+
+imgs, labels = next(train_batches)
+
+# This function wiLL plot images in the form of a grid with 1 row and 10 coLumns where images are plac
+def plotImages(images_arr):
+    fig, axes = plt.subplots(1, 10, figsize=(20,20))
+    axes = axes.flatten()
+    for img, ax in zip(images_arr, axes):
+        ax.imshow(img)
+        ax.axis('off')
+    plt.tight_layout()
+    plt.show()
+    
+    
+plotImages(imgs)
+print (labels)
+
