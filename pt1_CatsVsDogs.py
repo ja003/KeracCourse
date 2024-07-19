acffb2e3e5a9592660477c7355860016e4c16e8c
Adam Jurík <ja004@seznam.cz>
2024-07-19 12:37:14 +0200

    13. Convolutional Neural Network Predictions with TensorFlow's Keras API
    https://deeplizard.com/learn/video/bfQBPNDy5EM

--- pt1_CatsVsDogs.py
+++ pt1_CatsVsDogs.py
@@ -115,3 +115,57 @@ model.fit(x=train_batches,
 #     verbose=2
 # )
 
+
+test_imgs, test_labels = next(test_batches)
+plotImages(test_imgs)
+print(test_labels)
+
+test_batches.classes
+
+predictions = model.predict(x=test_batches, steps=len(test_batches), verbose=0)
+
+np.round(predictions)
+
+cm = confusion_matrix(y_true=test_batches.classes, y_pred=np.argmax(predictions, axis=-1))
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    
+test_batches.class_indices
+
+cm_plot_labels = ['cat','dog']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+
+
+
+
