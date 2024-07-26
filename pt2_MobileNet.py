279f01cad07e5fe95b1bd156f2c08df465b1a392
Adam Jurík <ja004@seznam.cz>
2024-07-26 11:10:06 +0200

    18. MobileNet Image Classification with TensorFlow's Keras API
    https://deeplizard.com/learn/video/OO4HD-1wRN8

---
+++ pt2_MobileNet.py
@@ -0,0 +1,55 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import Model
+from tensorflow.keras.applications import imagenet_utils
+from sklearn.metrics import confusion_matrix
+import itertools
+import os
+import shutil
+import random
+import matplotlib.pyplot as plt
+
+
+# 18. MobileNet Image Classification with TensorFlow's Keras API
+# https://deeplizard.com/learn/video/OO4HD-1wRN8
+
+mobile = tf.keras.applications.mobilenet.MobileNet()
+
+def prepare_image(file):
+    img_path = 'data/MobileNet-samples/'
+    img = image.load_img(img_path + file, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+
+from IPython.display import Image
+Image(filename='data/MobileNet-samples/1.PNG', width=300,height=200)
+
+# lizard
+preprocessed_image = prepare_image('1.PNG')
+predictions = mobile.predict(preprocessed_image)
+results = imagenet_utils.decode_predictions(predictions)
+results
+
+# espresso
+Image(filename='data/MobileNet-samples/2.PNG', width=300,height=200)
+
+preprocessed_image = prepare_image('2.PNG')
+predictions = mobile.predict(preprocessed_image)
+results = imagenet_utils.decode_predictions(predictions)
+results
+
+# strawberry
+Image(filename='data/MobileNet-samples/3.PNG', width=300,height=200)
+
+preprocessed_image = prepare_image('3.PNG')
+predictions = mobile.predict(preprocessed_image)
+results = imagenet_utils.decode_predictions(predictions)
+results
