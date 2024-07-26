02aca7bcf2fc5126d862faba4f58bb5d5f0744fb
Adam Jurík <ja004@seznam.cz>
2024-07-26 12:59:23 +0200

    20. Fine-Tuning MobileNet on Custom Data Set with TensorFlow's Keras API
    - AttributeError: 'NoneType' object has no attribute 'items'

--- pt2_MobileNet.py
+++ pt2_MobileNet.py
@@ -54,4 +54,107 @@ predictions = mobile.predict(preprocessed_image)
 results = imagenet_utils.decode_predictions(predictions)
 results
 
-# Process Images for Fine-Tuned MobileNet with TensorFlow's Keras API
+# 19. Process Images for Fine-Tuned MobileNet with TensorFlow's Keras API
+
+# Organize data into train, valid, test dirs
+os.chdir('data/Sign-Language-Digits-Dataset')
+if os.path.isdir('train/0/') is False: 
+    os.mkdir('train')
+    os.mkdir('valid')
+    os.mkdir('test')
+
+    for i in range(0, 10):
+        shutil.move(f'{i}', 'train')
+        os.mkdir(f'valid/{i}')
+        os.mkdir(f'test/{i}')
+
+        valid_samples = random.sample(os.listdir(f'train/{i}'), 30)
+        for j in valid_samples:
+            shutil.move(f'train/{i}/{j}', f'valid/{i}')
+
+        test_samples = random.sample(os.listdir(f'train/{i}'), 5)
+        for k in test_samples:
+            shutil.move(f'train/{i}/{k}', f'test/{i}')
+os.chdir('../..')
+
+train_path = 'data/Sign-Language-Digits-Dataset/train'
+valid_path = 'data/Sign-Language-Digits-Dataset/valid'
+test_path = 'data/Sign-Language-Digits-Dataset/test'
+
+train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+    directory=train_path, target_size=(224,224), batch_size=10)
+valid_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+    directory=valid_path, target_size=(224,224), batch_size=10)
+test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.mobilenet.preprocess_input).flow_from_directory(
+    directory=test_path, target_size=(224,224), batch_size=10, shuffle=False)
+
+# 20. Fine-Tuning MobileNet on Custom Data Set with TensorFlow's Keras API
+
+mobile = tf.keras.applications.mobilenet.MobileNet()
+mobile.summary()
+
+# take 5th to last layer
+x = mobile.layers[-5].output
+
+x = tf.keras.layers.Reshape(target_shape=(1024,))(x)
+output = Dense(units=10, activation='softmax')(x)
+
+model = Model(inputs=mobile.input, outputs=output)
+
+for layer in model.layers[:-22]:
+    layer.trainable = False
+    
+model.summary()
+    
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+    
+# !!! AttributeError: 'NoneType' object has no attribute 'items'
+model.fit(x=train_batches,
+            steps_per_epoch=len(train_batches),
+            validation_data=valid_batches,
+            validation_steps=len(valid_batches),
+            epochs=10,
+            verbose=2
+)
+
+test_labels = test_batches.classes
+
+predictions = model.predict(x=test_batches, steps=len(test_batches), verbose=0)
+
+cm = confusion_matrix(y_true=test_labels, y_pred=predictions.argmax(axis=1))
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')  
+
+
+test_batches.class_indices
